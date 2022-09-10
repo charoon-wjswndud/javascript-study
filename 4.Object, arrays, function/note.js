1f40fec7f77acd5eb7ba786960c18e5772f1e586
jeonjuyoung <wjswndud53@gmail.com>
2022-09-10 17:01:18 +0900

    update log.md

---
+++ 4.Object, arrays, function/note.js
@@ -0,0 +1,15 @@
+// function add1(x) {
+//     return ++x;
+// }
+// let a = 3;
+// let b = add1(a);
+// console.log(`a = ${a} b = ${b}`);
+
+// let a = "global";
+// function f(){
+//     let a = "local";
+//     console.log(a);
+// }
+// f();
+// console.log(a);
+
